0e873042f997c9c70a3412b7a00c097908e581f4
Gabriel de Oliveira Ostroski <gabriel.ostroski@ufpr.br>
2025-11-05 22:28:45 -0300

    definindo
    Define structures for patient and priority queue.

--- fila.h
+++ fila.h
@@ -1,1 +1,24 @@
+#ifndef fila_de_prioridades
+#define fila_de_prioridades
 
+//definir a estrutura para os pacientes
+struct paciente { 
+    char paciente[20];
+    int prioridade;
+} paciente;
+
+struct fila{
+    struct paciente *Paciente; // apontador para o vetor de pacientes
+    int tamanho; //tamanho atual da fila
+    int capacidade; //tamanho total da fila
+} fila;
+
+struct heap{
+    struct paciente a[20];
+    int tamanho;
+}heap;
+
+//função que inicia o heap
+void InicHeap (struct heap *Heap);
+
+#endif
