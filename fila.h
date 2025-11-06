5ec3921b9515f31b647f5e7d56146621caf57d45
Gabriel de Oliveira Ostroski <gabriel.ostroski@ufpr.br>
2025-11-05 23:49:10 -0300

    definicoes novas

--- fila.h
+++ fila.h
@@ -1,22 +1,25 @@
 #ifndef fila_de_prioridades
 #define fila_de_prioridades
 
+#define true 1
+#define false 0
+
+#define Maximo_Pacientes 1024
+#define Max_nome 20
+
 //definir a estrutura para os pacientes
 struct paciente { 
-    char paciente[20];
+    char paciente[Max_nome];
     int prioridade;
 } paciente;
 
-struct fila{
-    struct paciente *Paciente; // apontador para o vetor de pacientes
-    int tamanho; //tamanho atual da fila
-    int capacidade; //tamanho total da fila
-} fila;
-
 struct heap{
-    struct paciente a[20];
+    struct paciente vetor[Maximo_Pacientes];
     int tamanho;
 }heap;
+
+long long heap_cmp = 0;
+long long heap_swaps = 0;
 
 //função que inicia o heap
 void InicHeap (struct heap *Heap);
